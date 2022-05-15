6b743a822bc7cef415765be445344df2c8de07c4
RailJannatov <railcennetov@gmail.com>
2022-05-15 10:11:51 +0400

    FileStream

---
+++ FileStrem,FileIO/FileStrem,FileIO/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStrem_FileIO
+{
+    class Group
+    {
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ FileStrem,FileIO/FileStrem,FileIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileStrem_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student { Name = "Eli", Surname = "Eliyev", Degree = 51 };
+            Student s2 = new Student { Name = "Akif", Surname = "Quliyev", Degree = 80 };
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            Group g1 = new Group { Name = "Ape4", Students = students };
+
+            var json = JsonConvert.SerializeObject(g1);
+
+            using(StreamWriter streamWriter = new StreamWriter(@"C:\Users\Rail\Desktop\AzTU_tasks\FileStrem,FileIO\FileStrem,FileIO\Files\json1.json"))
+            {
+                streamWriter.Write(json);
+            }
+            string result;
+            using(StreamReader streamReader = new StreamReader(@"C:\Users\Rail\Desktop\AzTU_tasks\FileStrem,FileIO\FileStrem,FileIO\Files\json1.json"))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            var json1 = JsonConvert.DeserializeObject(result);
+
+            Console.WriteLine(json1);
+     
+
+        }
+    }
+}
